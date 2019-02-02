041dd1c596af18ffc13470c392d3fa5c3057f461
liciolentimo <lentimo@gmail.com>
2019-02-02 20:00:48 +0300

    Added id attribute

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,24 +1,43 @@
-public class Hero{
+import java.util.List;
+import java.util.ArrayList;
+
+public class Hero {
     private String mName;
     private int mAge;
     private String mPower;
     private String mWeakness;
-    public Hero(String name, int age, String power, String weakness){
+    private int mId;
+    private static List<Hero> heroList = new ArrayList<Hero>();
+
+    public Hero(String name, int age, String power, String weakness) {
         mName = name;
         mAge = age;
         mPower = power;
         mWeakness = weakness;
+        mId = heroList.size();
     }
-    public String getName(){
+
+    public String getName() {
         return mName;
     }
-    public int getAge(){
+
+    public int getAge() {
         return mAge;
     }
-    public String getPower(){
+
+    public String getPower() {
         return mPower;
     }
-    public String getWeakness(){
+
+    public String getWeakness() {
         return mWeakness;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public static List<Hero> all() {
+        return heroList;
     }
 }
--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -7,12 +7,14 @@ public class Squad {
     public String mCause;
     private static List<Squad> squadList = new ArrayList<Squad>();
     private List<Hero> mHeroes;
+    private int mId;
 
     public Squad(String name, int size, String cause) {
         mName = name;
         mSize = size;
         mCause = cause;
         mHeroes = new ArrayList<Hero>();
+        mId = squadList.size();
     }
 
     public String getName() {
@@ -31,13 +33,23 @@ public class Squad {
         squadList.clear();
     }
 
-    public static List<Squad> all(){
+    public static List<Squad> all() {
         return squadList;
     }
 
-    // public int getId() {
-    //     return mId;
-    // }
-   
+    public static Squad find(int id) {
+        return squadList.get(id - 1);
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void addHero(Hero hero){
+        mHeroes.add(hero);
+      }
+      public List<Hero> getHeros() {
+       return mHeroes;
+     }
 
 }
