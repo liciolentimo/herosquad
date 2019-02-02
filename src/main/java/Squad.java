0d222b5f19de1d5b36f314206e3bbe84516d85bf
liciolentimo <lentimo@gmail.com>
2019-02-02 19:29:40 +0300

    Added a getAll method to list all squads

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,20 +1,43 @@
-public class Squad{
+import java.util.List;
+import java.util.ArrayList;
+
+public class Squad {
     public String mName;
     public int mSize;
     public String mCause;
+    private static List<Squad> squadList = new ArrayList<Squad>();
+    private List<Hero> mHeroes;
 
-    public Squad(String name, int size, String cause){
+    public Squad(String name, int size, String cause) {
         mName = name;
         mSize = size;
         mCause = cause;
+        mHeroes = new ArrayList<Hero>();
     }
-    public String getName(){
+
+    public String getName() {
         return mName;
     }
-    public int getSize(){
+
+    public int getSize() {
         return mSize;
     }
-    public String getCause(){
+
+    public String getCause() {
         return mCause;
     }
+
+    public static void clear() {
+        squadList.clear();
+    }
+
+    public static List<Squad> all(){
+        return squadList;
+    }
+
+    // public int getId() {
+    //     return mId;
+    // }
+   
+
 }
