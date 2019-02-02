625b104500256d5ff8bdce06b94bdb27f4f8cc76
liciolentimo <lentimo@gmail.com>
2019-02-02 19:28:30 +0300

    Added routing to success page after squad entry

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -28,6 +28,25 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/squads", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("squads", Squad.all());
+      model.put("template", "templates/squadList.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/squads", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String name = request.queryParams("name");
+      int size = Integer.parseInt(request.queryParams("size"));
+      String cause = request.queryParams("cause");
+      Squad newSquad = new Squad(name, size, cause);
+      model.put("template", "templates/squadSuccess.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    
+
     // get("/squads", (request, response) -> {
     //   Map<String, Object> model = new HashMap<String, Object>();
     //   model.put("squads", Squad.all());
